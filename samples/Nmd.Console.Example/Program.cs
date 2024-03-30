e66470c048b908220e994f7357e2300b8f1e1189
Michiel Lankamp <michiel.lankamp@madaster.com>
2024-03-30 20:21:29 +0100

    Added an example

---
+++ samples/Nmd.Console.Example/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Hosting;
+using System.CommandLine.NamingConventionBinder;
+using System.CommandLine.Parsing;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using NMD.Api;
+
+var runner = BuildCommandLine()
+    .UseHost(_ => Host.CreateDefaultBuilder(args),
+        host =>
+        {
+            host.ConfigureServices((hostContext, services) =>
+            {
+                services.AddNmdApiClient(options =>
+                {
+                    options.ClientId = hostContext.Configuration.GetValue<string>("NMD:ClientId");
+                    options.ClientSecret = hostContext.Configuration.GetValue<string>("NMD:ClientSecret");
+                });
+            });
+        })
+    .Build();
+
+await runner.InvokeAsync(args);
+
+static CommandLineBuilder BuildCommandLine()
+{
+    var root = new RootCommand()
+    {
+        Handler = CommandHandler.Create<IHost>(async host =>
+        {
+            var client = host.Services.GetRequiredService<INMDClient>();
+            var response = await client.ElementsGetAsync(100, 0);
+            Console.WriteLine(response);
+        })
+    };
+
+    return new CommandLineBuilder(root);
+}
